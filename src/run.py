80c092de76ee5e65bce126fbdbe55a0f430f9b5a
Felix Portillo <felixp@pitagyro.ece.ucdavis.edu>
2019-12-05 14:28:02 -0800

    Quick fix to handle compatibility between python versions

--- src/run.py
+++ src/run.py
@@ -1,14 +1,14 @@
 import config as cfg
-from train_model import train_model
-from prune_model import prune_model
-from eval_model import eval_model
 
 def main():
     if cfg.model_train:
+        from train_model import train_model
         train_model()
     if cfg.model_eval:
+        from eval_model import eval_model
         eval_model()
     if cfg.model_prune:
+        from prune_model import prune_model
         prune_model()
 
 if __name__ == '__main__':
